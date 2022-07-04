c481470d1e3897b9e9b5caa5ab405dcba0e7b07c
Vitalii Guzeev <viviag@yandex.ru>
2022-07-04 14:04:48 +0300

    Make initdb script idempotent

--- initdb.sql
+++ initdb.sql
@@ -1,9 +1,9 @@
-CREATE TABLE objects (
+CREATE TABLE IF NOT EXISTS objects (
     id SERIAL PRIMARY KEY
   , name text NOT NULL
   , color text
   , background text
-  , orderc int NOT NULL
+  , orderc int UNIQUE NOT NULL
   , flag bool NOT NULL DEFAULT true
   );
 
@@ -14,4 +14,5 @@ INSERT INTO objects (name, color, background, orderc, flag)
        , ('name4', 'rgb(0,0,255)', null, 4, 't')
        , ('name5', 'rgb(255,0,255)', 'rgb(255,255,255)', 5, 't')
        , ('name6', 'rgb(0,255,255)', 'rgb(255,255,255)', 6, 'f')
-       , ('name7', 'rgb(255,255,0)', null, 7, 't');
+       , ('name7', 'rgb(255,255,0)', null, 7, 't')
+  ON CONFLICT DO NOTHING;
