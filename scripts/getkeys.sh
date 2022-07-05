dc7491740ab90adec8f288ec9aaeb3a469bd55a2
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2022-07-05 13:51:00 +0300

    curl -s

--- scripts/getkeys.sh
+++ scripts/getkeys.sh
@@ -7,11 +7,11 @@ getkey() {
 while read url;
 do
 echo '#' $url
-curl "$url.keys"
+curl -s "$url.keys"
 echo
 done
 }
 
 echo https://github.com/aeifn | getkey
 echo https://github.com/viviag | getkey
-curl https://api.github.com/repos/nikita-volkov/hasql/contributors | grep html_url | awk '{print $2}' | tr -d '",' | getkey
+curl -s https://api.github.com/repos/nikita-volkov/hasql/contributors | grep html_url | awk '{print $2}' | tr -d '",' | getkey
