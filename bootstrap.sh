24b173e6c9f521175556e1a236fbb446889e6586
Tester <tester@testing>
2022-07-05 13:29:13 -0400

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,21 @@
-sudo apt-get update &&
+echo installing for user $USER
+
+sudo apt-get update
+
+# Instal general and dev packages
 sudo apt-get install -y \
 tmux htop vim \
 strace valgrind gdb \
 tmuxinator \
-tcpdump \
-postgresql postgresql-client pgbouncer \
-# etc
+tcpdump
 
-sudo -u postgres createuser --no-password debian
-sudo -u postgres createdb -O debian debian
+# Postgres
+sudo apt-get install -y postgresql postgresql-client pgbouncer
+sudo service postgresql start
+sudo -u postgres createuser --no-password $USER
+sudo -u postgres createdb -O $USER $USER
 
+# Configure git
 git config --global user.email "tester@testing"
 git config --global user.name "Tester"
+git config pull.ff only
